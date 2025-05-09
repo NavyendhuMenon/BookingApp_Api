9a85f765a9efc9ec54df0c2b5d08c9d47b081f1e
Navyendhu <navyendhummenon@gmail.com>
2025-05-10 01:33:27 +0530

    login api tested

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,3 +25,34 @@ exports.register = async (req, res) => {
     res.status(500).json({ message: "Server Error" });
   }
 };
+
+exports.login = async (req, res) => {
+  console.log("data on login", req.body);
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({
+        message: "Login successful",
+        token,
+      });
+  } catch (err) {
+    res.status(500).json({ message: "Server Error" });
+  }
+};
